1eec500988d102ffc01bb80543f26b00ae213497
Michael Himmelstein KB3NZQ <himmelsmi@gmail.com>
2013-03-10 12:58:32 -0400

    working on changes

--- default.py
+++ default.py
@@ -1,6 +1,12 @@
 import urllib, urllib2, re, xbmcplugin, xbmcgui, xbmcaddon, os
 from time import strftime
 from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup
+import StorageServer
+from datetime import datetime
+try:
+    import json
+except:
+    import simplejson as json
 
 __settings__ = xbmcaddon.Addon(id='plugin.video.thegeekgroup')
 __language__ = __settings__.getLocalizedString
@@ -43,4 +49,28 @@ xbmcplugin.endOfDirectory(int(sys.argv[1]))
 # video_id = 'youtube_video_id'
 #playback_url = 'plugin://plugin.video.youtube/?action=play_video&videoid=%s' % video_id
 #item = xbmcgui.ListItem(path=playback_url)
-#xbmcplugin.setResolvedUrl(plugin_handle, True, item)
+#xbmcplugin.setResolvedUrl(plugin_handle, True, item) 
+def make_request(url):
+        try:
+            headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0',
+                       'Referer' : 'http://twit.tv'}
+            req = urllib2.Request(url,None,headers)
+            response = urllib2.urlopen(req)
+            data = response.read()
+            response.close()
+            return data
+        except urllib2.URLError, e:
+            addon_log( 'We failed to open "%s".' % url)
+            if hasattr(e, 'reason'):
+                addon_log('We failed to reach a server.')
+                addon_log('Reason: ', e.reason)
+            if hasattr(e, 'code'):
+                addon_log('We failed with error code - %s.' % e.code)
+
+		soup = BeautifulSoup(make_request('http://usher.justin.tv/find/thegeekgroup.xml?type=live'))
+        token = ' jtv='+soup.token.string.replace('\\','\\5c').replace(' ','\\20').replace('"','\\22')
+        rtmp = soup.connect.string+'/'+soup.play.string
+        Pageurl = ' Pageurl=http://www.justin.tv/thegeekgroup'
+        swf = ' swfUrl=http://www.justin.tv/widgets/live_embed_player.swf?channel=thegeekgroup live=true'
+        url = rtmp+token+swf+Pageurl
+        return url
